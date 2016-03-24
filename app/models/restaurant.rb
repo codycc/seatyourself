5e38ac56f141d26f707ab80ae2e2d79b41bf5a70
naemcivic <naemcivic@gmail.com>
2016-03-24 19:03:50 -0400

    update rdoc

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ActiveRecord::Base
   has_many :reservations
   has_many :users, through: :reservations
   has_many :reviews
-  has_many :user, through: :reviews
+  has_many :users, through: :reviews
 
   def available?(party_size,time)
      available_capacity = capacity - reservations.where('time >= ? and time < ?', time.beginning_of_hour, time.end_of_hour).sum(:party_size)
